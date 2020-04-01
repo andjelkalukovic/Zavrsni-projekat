f9782c892bff0a9e06eb35564544207c0359e747
andjelkalukovic <60389145+andjelkalukovic@users.noreply.github.com>
2020-04-01 23:22:11 +0200

    Delete apiService.js

--- src/service/apiService.js
+++
@@ -1,9 +0,0 @@
-// import axios from 'axios'
-
-// // const proxyurl = "https://cors-anywhere.herokuapp.com/"
-// const getAllCocktails = async () =>{
-//     let response= await axios.get (`http://www.7timer.info/bin/api.pl?lon=113.17&lat=23.09&product=astro&output=xml`);
-//     console.log(response);
-//     return response.data;
-// }
-// export {getAllCocktails};
